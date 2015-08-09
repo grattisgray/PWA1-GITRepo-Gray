c471aa686ef3366da345a133da2058b07d2273e4
grattisgray <mggray@fullsail.edu>
2015-08-08 21:22:05 -0500

    Completed JavaScript

--- Wk1/goal1_assign_duel1-2/js/main.js
+++ Wk1/goal1_assign_duel1-2/js/main.js
@@ -9,9 +9,32 @@ Assignment: Goal1: Assignment: Analyze Duel #1
  */
 
 
+//Pseudo code
+
+/*alert user of the fight between Spider-man and Batman
+
+    set each fighters health to 100
+    damage is determine by a random number no greater than 20
+
+    after each round alert the use with
+
+        the health of each fighter
+        the round of the fight
+
+    after each round each fighter inflicts damage upon the other
+
+        set at a random number
+
+    alert the user of the winner of the fight
+
+        if both reach 0 health they both die
+        else which ever fight has health greater than zero wins the fight.
+ */
+
+//****** self executing function ******//
 (function() {
 
-    console.log("Fight!");
+    //console.log("Fight!");
 
 // player names
 
@@ -30,7 +53,7 @@ Assignment: Goal1: Assignment: Analyze Duel #1
 
 // keeps track of the round for the fight
 
-    var round = 1;
+    var round = 0;
 
 //Fight Function
 
@@ -38,6 +61,7 @@ Assignment: Goal1: Assignment: Analyze Duel #1
         ///Fight Function Code///
         alert(playerOneName + ":" + playerOneHealth + " *START* " + playerTwoName + ":" + playerTwoHealth);
 
+        //for loop to determine damage inflicted against each fighter and winner of the fight
         for (var i=0; i<10; i++){
             //random formula is - Math.floor(Math.random() * (max - min) + min);
            //random damage variables and formulas for each fighter
@@ -49,19 +73,56 @@ Assignment: Goal1: Assignment: Analyze Duel #1
             //console.log(fighter1);
             //console.log(fighter2);
 
+            //players inflict damage each round
+            //damage infliction formula
+
+            playerOneHealth -= fighter1;
+            playerTwoHealth -= fighter2;
+
+            //console.log(playerOneHealth);
+            //console.log(playerTwoHealth);
+
+            //variable for checking the winner
+            var results = winnerCheck();
+           // console.log(results);
+
+            //alert the user of the winner or of a tie
+            if(results === "no winner") {
+                round++;
+                alert(playerOneName + ":" + playerOneHealth + " *ROUND " + round + " OVER* " + playerTwoName + ":" + playerTwoHealth);
+            }else{
+                //alert the user with the results of the fight
+                alert(results);
+                //break the loop if they die
+                break
+            }
         };
-        winnerCheck()
+
     };
 
 // Check the Winner of the Fight Function
 
     function winnerCheck(){
+      //  console.log("In winnerCheck FN");
         ///Function Code///
+        //Result for no winner
+        var result = "no winner";
 
+        //if loop to determine the result
+
+        if(playerOneHealth <1 && playerTwoHealth <1){
+            result = "You Both Die";
+        }else if(playerOneHealth<1){
+            result = playerTwoName + " WINS!!!"
+        }else if(playerTwoHealth<1){
+            result = playerOneName + " WINS!!!"
+        };
+
+        return result;
     };
 
     /***** The Program Starts *****/
-    console.log("programs starts here");
+   // console.log("programs starts here");
     fight();
 
 })();
