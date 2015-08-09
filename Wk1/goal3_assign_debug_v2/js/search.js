f7b8dce4ff23c6c06ffe6b42c560fe2da20ae025
grattisgray <mggray@fullsail.edu>
2015-08-09 15:42:48 -0500

    Pseudo code written

--- Wk1/goal3_assign_debug_v2/js/search.js
+++ Wk1/goal3_assign_debug_v2/js/search.js
@@ -47,51 +47,64 @@ Else search the database for the term
 		;
 		
 		// Check search length, must have 3 characters
+			// String must be three or more characters long
+			// If loop checks the length of the user's stringer
 		if(query.length < 3){
+			// Alert the user with an error message
 			alert("Your search query is too small, try again.);
 			
 			// (DO NOT FIX THE LINE DIRECTLY BELOW)
+			// Return the search results to the user
 			searchInput.focus();
 			return;
 		};
-		
+		// Search function goes here
 		search(query);
 	};
 	
 	// Finds search matches
+		// Function searches for matches to the user's string
 	var search = function(query)
 		
 		// split the user's search query string into an array
+		// create an array using the user's string of characters
 		var queryArray = query.join(" ");
 		
 		// array to store matched results from database.js
+		// store the matches for the results in an array (taken from the db js file)
 		var results = [];
 
 		// loop through each index of db array
+		// use a for loop to search each index in the array to find matches
 		for(var i=0, j=db.length; i<j; i++){
 		
 			// each db[i] is a single video item, each title ends with a pipe "|"
 			// save a lowercase variable of the video title
+			// Database indexes will end with a pipe "|"
 			var dbTitleEnd = db[i].indexOf('|');
 			var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
 			
 			// loop through the user's search query words
 			// save a lowercase variable of the search keyword
+			// use a for loop to search through the words and save as lowercase variables
 			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
 				var qitem = queryArray[ii].tolowercase();
 				
 				// is the keyword anywhere in the video title?
 				// If a match is found, push full db[i] into results array
+				// Use if loop to find all the matches and display the results
 				var compare = dbitem.indexOf(qitem);
 				if(compare !== -1){
+					// push the results to webpage
 					results.push(db[i]);
 				};
 			;
 		;
-		
+		// Sort all the results by keywords
 		results.sort();
 		
 		// Check that matches were found, and run output functions
+		// Check for matches, and display results
 		if(results.length = 0){
 			noMatch();
 		}else{
@@ -100,15 +113,18 @@ Else search the database for the term
 	};
 	
 	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
+		// If no results are found display a message to the user with the proper HTML tags and CSS styles
 	var noMatch = function(){
 		var html = ''+
 			'<p>No Results found.</p>'+
 			'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
 		;
+		// Results will be displayed in the HTML
 		resultsDIV.innerHTML = html;
 	};
 	
 	// Put matches into page as paragraphs with anchors
+		// Display the results as HTML paragraphs and anchor them with their title and url
 	var showMatches = function(results){
 		
 		// THE NEXT 4 LINES ARE CORRECT.
@@ -118,17 +134,21 @@ Else search the database for the term
 		;
 		
 		// loop through all the results search() function
+		// use for loop to gather all the results
 		for(var i=0, j=results.length; i<j; i++){
 		
 			// title of video ends with pipe
 			// pull the title's string using index numbers
+			// using index numbers pull the title's string from the db
 			titleEnd = results[i].indexOf('|');
 			title = results[i].subString(0, titleEnd);
 			
 			// pull the video url after the title
+			// after the title has been pulled, pull the url from the db
 			url = results[i].substring(results[i].indexOf('|')+1, results[i].length);
 			
 			// make the video link - THE NEXT LINE IS CORRECT.
+			// display the url as a link
 			html += '<p><a href=' + url + '>' + title + '</a></p>';
 		};
 		resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
